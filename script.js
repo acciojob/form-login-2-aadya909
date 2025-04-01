b894a442921895fc98785ba334a167c20fe1830c
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-04-01 23:54:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+function showalert(event){
+	event.preventDefault();
+	 let firstName = document.getElementById("firstName").value;
+     let lastName = document.getElementById("lastName").value;
+     let phoneNumber = document.getElementById("phoneNumber").value;
+  let emailId = document.getElementById("emailId").value;
+	 let message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`;
+            alert(message);
+}
