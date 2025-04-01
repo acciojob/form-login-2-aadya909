ef3000414031302506c7a7894ac942a292c9465d
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-04-01 23:57:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function showalert(event){
-	event.preventDefault();
-	 let firstName = document.getElementById("firstName").value;
-     let lastName = document.getElementById("lastName").value;
-     let phoneNumber = document.getElementById("phoneNumber").value;
-  let emailId = document.getElementById("emailId").value;
-	 let message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`;
+ function showAlert(event) {
+            event.preventDefault(); // Prevents form submission
+            
+            let firstName = document.getElementById("firstName").value;
+            let lastName = document.getElementById("lastName").value;
+            let phoneNumber = document.getElementById("phoneNumber").value;
+            let emailId = document.getElementById("emailId").value;
+            
+            let message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`;
             alert(message);
-}
+        }
